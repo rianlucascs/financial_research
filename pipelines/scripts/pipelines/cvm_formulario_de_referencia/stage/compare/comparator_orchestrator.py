163b06bdd8f30cc2369a3e0dcc663ff8682239ad
rianlucascs <rlucasgit@gmail.com>
2026-08-26 13:58:53 -0300

    feat(cvm_formulario_de_referencia): adicionar compare e retention

---
+++ pipelines/scripts/pipelines/cvm_formulario_de_referencia/stage/compare/comparator_orchestrator.py
@@ -0,0 +1,31 @@
+
+
+from pipelines.shared.context import PipelineContext
+from pipelines.shared.interfaces.pipelines.stage.compare.comparator_orchestrator import ComparatorOrchestratorInterface
+from pipelines.shared.interfaces.pipelines.stage.compare.comparator_workers import ComparatorWorkersInterface
+
+from pipelines.scripts.pipelines.cvm_formulario_de_referencia.stage.compare.comparator_worker_A import ComparatorWorkerA
+
+
+class ComparatorOrchestrator(ComparatorOrchestratorInterface):
+
+
+    def __init__(
+        self,
+        *,
+        pipeline: str,
+    ) -> None:
+        
+        super().__init__(
+            pipeline=pipeline
+        )
+    
+    
+    def _build_workers(self, ctx: PipelineContext) -> list[ComparatorWorkersInterface]:
+        """
+        Método responsável por construir os workers de comparação.
+        """
+        
+        return [
+            ComparatorWorkerA(pipeline=self.pipeline)
+        ]
---
+++ pipelines/scripts/pipelines/cvm_formulario_de_referencia/stage/compare/comparator_worker_A.py
@@ -0,0 +1,52 @@
+"""
+Worker:
+    comparator_worker_A
+    
+Responsabilidades:
+    ...
+    
+Notas:
+    
+"""
+
+ 
+from pipelines.shared.context import PipelineContext
+from pipelines.shared.interfaces.pipelines.stage.compare.comparator_worker_A import ComparatorWorkerInterfaceA
+
+from pathlib import Path
+
+
+class ComparatorWorkerA(ComparatorWorkerInterfaceA):
+
+
+    def __init__(
+        self,
+        *,
+        pipeline: str,
+    ) -> None:
+        
+        super().__init__(
+            pipeline=pipeline
+        )
+        
+        
+    def _build_previous_data_path(self, ctx: PipelineContext) -> Path:
+        
+        return ctx.build_transformed_path(
+            ctx.current_snapshot_path(self.pipeline, self.previous_snapshot),
+            subdir_stage="to_processed",
+            subdir_format="parquet"
+        )
+    
+    
+    def _build_current_data_path(self, ctx: PipelineContext) -> Path:
+        
+        return ctx.build_transformed_path(
+            ctx.current_snapshot_path(self.pipeline, self.current_snapshot),
+            subdir_stage="to_processed",
+            subdir_format="parquet"
+        )
+        
+    
+    def _key_cols(self) -> list[str]:
+        return ["CD_CVM"]
